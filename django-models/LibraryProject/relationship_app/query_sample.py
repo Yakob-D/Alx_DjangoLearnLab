bff9172c82bf5ec22f0fbc25a7ad55345de889ab
Yakob <126866800+Yakob-D@users.noreply.github.com>
2025-02-22 17:47:24 +0300

    Update query_sample.py

--- django-models/LibraryProject/relationship_app/query_sample.py
+++ django-models/LibraryProject/relationship_app/query_sample.py
@@ -2,12 +2,12 @@ from .models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 author_name = 'James'
-author = Author.objects.get(name=author_name)  # Get the Author instance
+author = Author.objects.get(name=author_name)  # Ensure the author exists
 books_by_author = Book.objects.filter(author=author)  # Filter books by the Author instance
 
 # List all books in a library
 library_name = 'Library Name'
-library = Library.objects.get(name=library_name)  # Get the Library instance
+library = Library.objects.get(name=library_name)  # Ensure the library exists
 books_in_library = library.books.all()  # Get all books in the library
 
 # Retrieve the librarian for a library
