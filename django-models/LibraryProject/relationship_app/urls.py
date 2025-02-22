5308cd35b09bdc8df3aaedb5ad8b3241934fe376
Yakob <dibabuyakob@gmail.com>
2025-02-22 19:29:19 +0300

    added views.register

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,13 @@
 # urls.py
 from django.urls import path
-from .views import list_books, LibraryDetailView
-from .views import register  # Ensure user_register is imported
+from . import views  # Import the views module
+from .views import list_books, LibraryDetailView  # Import specific views
 from django.contrib.auth.views import LoginView, LogoutView  # Import built-in views
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),  # Use Django's built-in LoginView
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),  # Use Django's built-in LogoutView
-    path('register/', register, name='register'),  # Keep your custom user_register view
+    path('register/', views.register, name='register'),  # Use views.register for the custom user_register view
     path('books/', list_books, name='list_books'),  # URL for function-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # URL for class-based view
 ]
