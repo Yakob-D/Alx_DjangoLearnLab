330d39b3645a83f3310bc96b1cc524c7c09912cd
Yakob <126866800+Yakob-D@users.noreply.github.com>
2025-02-22 17:50:18 +0300

    Update query_sample.py

--- django-models/LibraryProject/relationship_app/query_sample.py
+++ django-models/LibraryProject/relationship_app/query_sample.py
@@ -1,29 +1,23 @@
 from .models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
-author_name = 'James'
+author_name = 'James'  # Replace this with the actual author's name in your database
 try:
-    # Get the Author instance
-    author = Author.objects.get(name=author_name)
-    books_by_author = Book.objects.filter(author=author)  # Filter books by the Author instance
+    author = Author.objects.get(name=author_name)  # Get the Author instance
+    books_by_author = Book.objects.filter(author=author)  # Query books by the author
 except Author.DoesNotExist:
-    books_by_author = []  # Handle case where author doesn't exist
-    print(f"Author '{author_name}' does not exist.")
+    books_by_author = []  # No books if the author doesn't exist
 
 # List all books in a library
-library_name = 'Library Name'
+library_name = 'Library Name'  # Replace with the actual library's name
 try:
-    # Get the Library instance
-    library = Library.objects.get(name=library_name)
-    books_in_library = library.books.all()  # Get all books associated with the Library instance
+    library = Library.objects.get(name=library_name)  # Get the Library instance
+    books_in_library = library.books.all()  # Query all books in the library
 except Library.DoesNotExist:
-    books_in_library = []  # Handle case where library doesn't exist
-    print(f"Library '{library_name}' does not exist.")
+    books_in_library = []  # No books if the library doesn't exist
 
 # Retrieve the librarian for a library
 try:
-    # Get the Librarian instance associated with the Library instance
-    librarian = Librarian.objects.get(library=library)
+    librarian = Librarian.objects.get(library=library)  # Get the Librarian instance
 except Librarian.DoesNotExist:
-    librarian = None  # Handle case where librarian doesn't exist
-    print(f"No librarian found for the library '{library_name}'.")
+    librarian = None  # No librarian if the library doesn't exist
