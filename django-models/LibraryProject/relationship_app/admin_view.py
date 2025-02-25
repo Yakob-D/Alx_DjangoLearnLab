3969d6fba319ca0b54cf7593b0974d467c614ae0
Yakob <dibabuyakob@gmail.com>
2025-02-25 17:32:02 +0300

    fixed some issues with the admin_view

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
 def is_admin(user):
-    return user.is_authenticated and user.userprofile.role == 'Admin'
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
 @user_passes_test(is_admin)
 def admin_view(request):
