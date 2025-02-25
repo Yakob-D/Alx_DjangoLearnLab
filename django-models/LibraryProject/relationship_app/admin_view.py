0605e71356ab2a0559de06beda2782a5927acae5
Yakob <dibabuyakob@gmail.com>
2025-02-25 19:08:26 +0300

    project 3

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
 def is_admin(user):
-    return user.is_authenticated and user.userprofile.role == 'Admin'
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
 @user_passes_test(is_admin)
 def Admin(request):
