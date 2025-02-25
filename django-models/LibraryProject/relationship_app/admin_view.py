b2f5ca21a8a0988557b3cb9b7dbaacde8f623cbf
Yakob <dibabuyakob@gmail.com>
2025-02-25 19:03:35 +0300

    minor bug fix

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -4,6 +4,6 @@ from django.contrib.auth.decorators import user_passes_test
 def is_admin(user):
     return user.is_authenticated and user.userprofile.role == 'Admin'
 
-@user_passes_test()
+@user_passes_test(is_admin)
 def Admin(request):
     return render(request, 'relationship_app/admin_view.html')
--- django-models/LibraryProject/relationship_app/librarian_view.py
+++ django-models/LibraryProject/relationship_app/librarian_view.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
-def is_admin(user):
+def is_librarian(user):
     return user.is_authenticated and user.userprofile.role == 'Librarian'
 
-@user_passes_test
+@user_passes_test(is_librarian)
 def Librarian(request):
     return render(request, 'relationship_app/librarian_view.html')
