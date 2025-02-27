bdfb178bac5d4eff3855c4756a518486fab5ff32
Yakob <dibabuyakob@gmail.com>
2025-02-27 11:54:22 +0300

    models.py update

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.conf import settings  # Import settings to use AUTH_USER_MODEL
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -9,3 +11,26 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, date_of_birth=None, profile_photo=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, date_of_birth=date_of_birth, profile_photo=profile_photo)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        user = self.create_user(username=username, email=email, password=password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()
