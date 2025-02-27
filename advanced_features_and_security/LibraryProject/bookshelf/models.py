2f7adb8d76909206e4ecabfab2a241694474ad6a
Yakob <dibabuyakob@gmail.com>
2025-02-27 11:48:28 +0300

    updated model to reference CustomUser

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.conf import settings  # Import settings to use AUTH_USER_MODEL
 
-# Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length = 200)
-    author = models.CharField(max_length = 100)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='books', null=True, blank=True)  # Reference to the custom user model
+
+    def __str__(self):
+        return self.title
