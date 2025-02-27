fdabb03d423f1f528eee506ae3030723689ad537
Yakob <dibabuyakob@gmail.com>
2025-02-27 19:07:11 +0300

    forms file update

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -4,4 +4,7 @@ from .models import Book
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['title', 'author', 'published_date']  # Add relevant fields
+        fields = ['title', 'author', 'published_date']
+
+class ExampleForm(forms.Form):  # Create the ExampleForm
+    user_input = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Enter your text'}))
