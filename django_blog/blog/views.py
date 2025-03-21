3d85dde054d6752e7e522c4c957c9554e91993bb
Yakob <dibabuyakob@gmail.com>
2025-03-21 19:28:40 +0300

    Created a view where users can update their emails, along with UserUpdateForm to help do that.

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,19 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import View
+from django.shortcuts import render, redirect
+from .forms import UserUpdateForm
 
-class ProfileView(LoginRequiredMixin, TemplateView):
+class ProfileView(LoginRequiredMixin, View):
     template_name = 'blog/profile.html'
+
+    def get(self,request):
+        form = UserUpdateForm(instance=request.user)
+        return render(request, self.template_name, {'form':form})
+
+    def post(self, request):
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+
+        return render(request, self.template_name, {'form':form})
