fa3d813e274fb572f7ae2e37d01d52205e6dfc30
Yakob <dibabuyakob@gmail.com>
2025-03-22 11:15:45 +0300

    Created Views for CRUD operations for blogs.

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Post
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -13,3 +14,17 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['email']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+    
+    def save(self, commit=True, user=None):
+        post = super().save(commit=False)
+
+        if user:
+            post.author = user
+        if commit:
+            post.save()
+        return post
--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.ProfileView.as_view(), name = 'profile'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
