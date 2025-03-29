171a2c978bbd077f8d2364e05d45ab7712f92675
Yakob <dibabuyakob@gmail.com>
2025-03-29 17:23:53 +0300

    added FeekView into the my posts app views

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostFeedViewSet, PostViewSet
+from .views import PostFeedViewSet, PostViewSet, FeedView
 
 # Set up the router for viewsets
 router = DefaultRouter()
@@ -13,4 +13,5 @@ router.register(r'feed', PostFeedViewSet, basename='feed')
 
 urlpatterns = [
     path('', include(router.urls)),  # Include the router URLs
+    path('feed/', FeedView.as_view(), name='feed'),
 ]
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,12 +1,35 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, generics
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import status
-
 from rest_framework import filters
 from rest_framework.pagination import PageNumberPagination
+from django.contrib.auth import get_user_model
+
+
+CustomUser = get_user_model()
+
+class FeedView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        following_users = request.user.following.all()  # Get users the current user follows
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')  # Get posts from followed users
+
+        posts_data = [
+            {
+                "id": post.id,
+                "author": post.author.username,
+                "title": post.title,
+                "content": post.content,
+                "created_at": post.created_at,
+            }
+            for post in posts
+        ]
+
+        return Response(posts_data)
 
 class PostPagination(PageNumberPagination):
     page_size = 10
