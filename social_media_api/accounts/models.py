e24d14f4988ad0fe7b75dddeec76e6f53ffe7177
Yakob <dibabuyakob@gmail.com>
2025-03-29 16:51:50 +0300

    updated user model to have following attribute while keeping the others

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,9 +1,9 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    # Define the following relationship
+    bio = models.TextField()
+    profile_picture = models.ImageField()
+    followers = models.ManyToManyField('self', symmetrical=False)
     following = models.ManyToManyField('self', symmetrical=False, related_name='followers', blank=True)
 
-    def __str__(self):
-        return self.username
