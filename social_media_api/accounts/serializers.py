6ab4ee2a5c8dd05631bbe600aef37d5a158878fc
Yakob <dibabuyakob@gmail.com>
2025-03-29 16:22:38 +0300

    included serializer.CharField() in serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -8,7 +8,7 @@ User = get_user_model()
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
-
+    passw = serializers.CharField()
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'password2']
